17839c655c0710971af22dcad3bf3d1c471372be
salman-cheema <33802734+salman-cheema@users.noreply.github.com>
2020-06-05 16:07:39 +0500

    Delete connection.py

--- connection.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-def postgres_connection():
-    db_string = "postgresql://postgres:34201800@sftp.cftvarydd1mo.us-east-1.rds.amazonaws.com:5432/postgres"
-    engine = create_engine(db_string)
-    return engine
